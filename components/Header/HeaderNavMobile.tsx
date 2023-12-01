ca36d569e604aff587e76c55f457cb17ba7cea12
vhamet <valentin.hamet@gmail.com>
2023-12-01 15:14:10 +0100

    setup fontawesome & maj animated burger menu

--- components/Header/HeaderNavMobile.tsx
+++ components/Header/HeaderNavMobile.tsx
@@ -3,6 +3,12 @@
 import { useState } from "react";
 import { signOut } from "next-auth/react";
 import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faArrowRightFromBracket,
+  faUser,
+  faBook,
+} from "@fortawesome/free-solid-svg-icons";
 
 import AnimatedBurgerMenu from "@/components/AnimatedBurgerMenu";
 import UserAvatar from "@/components/Header/UserAvatar";
@@ -20,13 +26,13 @@ const HeaderNavMobile = ({ user }: HeaderNavMobileProps) => {
   const [open, setOpen] = useState(false);
 
   return (
-    <div className="sm:hidden flex left-4 top-4">
-      <div className={cn("right-12 top-4 z-10", open ? "fixed" : "absolute")}>
+    <div className="sm:hidden left-4 top-4 flex">
+      <div className={cn("right-12 top-5 z-10", open ? "fixed" : "absolute")}>
         <AnimatedBurgerMenu onClick={() => setOpen(!open)} />
       </div>
       <aside
         className={cn(
-          "w-64 h-screen py-20 px-4 fixed top-0 right-0 bg-background-light duration-500",
+          "w-64 h-screen py-20 px-4 fixed top-0 right-0 bg-background-light border-l border-border duration-500",
           open ? "translate-x-0" : "translate-x-full"
         )}
       >
@@ -35,29 +41,26 @@ const HeaderNavMobile = ({ user }: HeaderNavMobileProps) => {
             <label className="cursor-pointer text-sm">{user.username}</label>
             <UserAvatar user={user} />
           </div>
-          <Link href="/profile">Profile</Link>
-          <Link href="/recipes">My Recipes</Link>
-          <div className="w-full mt-16 text-center">
-            <Button
-              type="button"
-              variant="ghostPrimary"
-              onClick={() => signOut()}
-            >
-              Sign Out
-            </Button>
-          </div>
+          <Link href="/profile" className="flex items-center gap-x-2">
+            Profile
+            <FontAwesomeIcon icon={faUser} />
+          </Link>
+          <Link href="/recipes" className="flex items-center gap-x-2">
+            My Recipes
+            <FontAwesomeIcon icon={faBook} />
+          </Link>
+          <Button
+            type="button"
+            variant="ghost"
+            className="p-0 flex gap-x-2 text-base text-primary"
+            onClick={() => signOut()}
+          >
+            Sign Out
+            <FontAwesomeIcon icon={faArrowRightFromBracket} />
+          </Button>
         </nav>
       </aside>
     </div>
-
-    /* transition: translate var(--animation-timing);
-  translate: -100%;
-  padding: 0.5rem 1rem;
-  padding-top: calc(var(--hamburger-height) + var(--hamburger-margin) + 1rem);
-  background-color: var(--foreground);
-  color: var(--background);
-  max-width: 10rem;
-  min-height: 100vh; */
   );
 };
 
